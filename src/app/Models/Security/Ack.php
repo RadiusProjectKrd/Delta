1c6afd8557b968a22ec8997b23efaaf085f9ca23
Ult1mateXPHP <alex.ultimatex@gmail.com>
2025-06-13 00:04:31 +0300

    alarm broadcast

--- src/app/Models/Security/Ack.php
+++ src/app/Models/Security/Ack.php
@@ -26,8 +26,10 @@ class Ack extends Model
         $alarm = Alarm::getAlarm($alarm_id);
         $owner_id = UserObjects::searchUserByObject($alarm->object_id);
         $owner = UnderSecurity::getUnderSecurityUser($owner_id->user_id);
+        $text = $user->first_name.' '.$user->last_name.' отреагировал на сигнал тревоги по обьекту '.$alarm->object_id;
         $api->response(
-            $api->builder($user->first_name.' '.$user->last_name.' отреагировал на сигнал', $owner->telegram_id)
+            $api->builder($text, $owner->telegram_id)
         );
+        $api->broadcast($text);
     }
 }
